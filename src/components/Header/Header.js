327b4e8330fac1ddfb793fc7e81c5cc1c78526b7
swopnil <swopnil420@gmail.com>
2020-10-09 21:38:55 +0600

    Making header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header-left">
+                {/* Avatars for logged in user */}
+                {/* Time icon */}
+            </div>
+            <div className="header-search">
+                {/* Search Icon */}
+                {/* Input */}
+            </div>
+            <div className="header-right">
+                {/* Help icon */}
+            </div>
+        </div>
+    );
+}
+
+export default Header;
